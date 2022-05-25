c816b84557da864646921f11747371c70c38f69f
Julian Gregoraschuk <juliangregoraschuk1@gmail.com>
2022-05-25 11:26:21 -0300

    IntroToCs homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,23 @@
 'use strict'
 
-function BinarioADecimal(num) {
+function BinarioADecimal(num) {    // D*2**I
   // tu codigo aca
-
+  let sumatoria = 0
+  for (let i = 0; i < num.length; i++) {
+    sumatoria = sumatoria + num[i] * 2 ** (num.length -1 -i) 
+  }
+  return sumatoria
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+let str = "";
+while(num > 0){
+  let resto = num % 2;
+  str = resto + str;
+  num = Math.floor(num / 2);
+}
+return str;
 }
 
 
